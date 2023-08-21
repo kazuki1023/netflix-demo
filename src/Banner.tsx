fa889076a849bafc8e538463e468ae7452a90bac
kazuki1023 <k.keio12569@gmail.com>
2023-08-21 17:10:06 +0900

    bannerにcssつけた

--- src/Banner.tsx
+++ src/Banner.tsx
@@ -1,5 +1,6 @@
 import  { useState, useEffect } from "react";
 import axios from "./axois.js";
+import "./Banner.css";
 import {requests} from "./request.js";
 type movieProps = {
   title?: string;
