a8abbee67f0e7dab2fa616d8aa88aa27b35ab5ed
kazuki1023 <k.keio12569@gmail.com>
2023-08-21 16:50:20 +0900

    delete不要console

--- src/request.js
+++ src/request.js
@@ -1,5 +1,4 @@
 const API_KEY = process.env.REACT_APP_API_KEY // TMDBのAPIKeyを入れる
-console.log(API_KEY)
 
 export const requests ={
     feachTrending:`/trending/all/week?api_key=${API_KEY}&language=en-us`,
